6feb81ba0d9d38304634c9c6170b5e7907c13b25
aumnatthacha <96460324+aumnatthacha@users.noreply.github.com>
2023-09-06 21:19:14 +0700

    Add Restaurant api for Cyclic

---
+++ config/dbconfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host:"aws.connect.psdb.cloud",
+    user:"5u8lscmzyq5sjneb2iz2",
+    password:"pscale_pw_FbTBWULyTgeVMtNS1aX1E8LZ3UqQgLHFvJNOBkqrjWT",
+    db:"se_database"
+}
