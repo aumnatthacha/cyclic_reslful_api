757618dbb4695f922552e9cf03562d4763b08298
aumnatthacha <96460324+aumnatthacha@users.noreply.github.com>
2023-09-11 10:14:44 +0700

    .

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,6 +1,14 @@
 module.exports = {
     host:"aws.connect.psdb.cloud",
-    user:"5u8lscmzyq5sjneb2iz2",
-    password:"pscale_pw_FbTBWULyTgeVMtNS1aX1E8LZ3UqQgLHFvJNOBkqrjWT",
+    user:"oyu2xzzn84ywzk95rc3d",
+    password:"pscale_pw_en6tRDb0z7TFCz8gqumwig6abIj8AhJNm861d673CFg",
     db:"se_database"
 }
+// require('dotenv').config()
+
+// module.exports = {
+//     HOST: process.env.HOST,
+//     USER: process.env.USER,
+//     PASSWORD: process.env.PASSWORD,
+//     DB: process.env.DB,
+// };
