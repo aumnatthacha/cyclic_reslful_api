7e2a709269d4bcbb16801a861d960326f85528de
aumnatthacha <96460324+aumnatthacha@users.noreply.github.com>
2023-09-28 14:21:14 +0700

    docker

--- model/db.js
+++ model/db.js
@@ -1,26 +1,27 @@
-const {Sequelize} = require("sequelize");
+const { Sequelize } = require("sequelize");
 const dbConfig = require("../config/dbconfig");
-
-//Create sequelize instance
-const sequelize = new Sequelize(dbConfig.db, dbConfig.user, dbConfig.password, {
-    host:dbConfig.host,
-    dialect:"postgres",
-    dialectOptions: {
-      ssl: {
-          require: true,
-          rejectUnauthorized: false
-      }
-   },
-});
+require('dotenv').config()
+//Create sequelize instance\
+const sequelize = new Sequelize("postgresql://postgres:example@localhost:5432/restaurant?schema=public");
+// const sequelize = new Sequelize(process.env.DATABASE_URL, "postgres", "example", {
+//   host: dbConfig.host,
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   },
+// });
 
 //Test the database 
-async function testConnection(){
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 }
 
 //
